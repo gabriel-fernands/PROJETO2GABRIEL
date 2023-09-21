118c1ea6a4e3259f38fc09e4ec3579cc7c1a2fa5
Gabriel Fernandes <mendesgabrielfernandes@gmail.com>
2023-09-21 18:50:52 -0300

    calculo dolar

---
+++ calculo dolar.java
@@ -0,0 +1,11 @@
+package maratona.alura.java;
+
+public class projeto02CalculaDolar {
+    public static void main(String[] args) {
+         double reais = 1950;
+         double dolar = 5.35;
+         dolar = reais / dolar; {
+            System.out.println("voce pode compra " +dolar +"dolar" );
+        }
+    }
+}
