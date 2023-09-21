aa2e8c180539b2694aba32968bc42940440df8aa
Gabriel Fernandes <mendesgabrielfernandes@gmail.com>
2023-09-21 19:17:52 -0300

    laço de repetição while

---
+++ alurawhile.java
@@ -0,0 +1,12 @@
+package maratona.alura.java;
+
+public class Aula01AluraWhile {
+    public static void main(String[] args) {
+                long contador = 1;
+                while (contador <= 10) {
+                    System.out.println(contador);
+                    contador++;
+                }
+            }
+        }
+
